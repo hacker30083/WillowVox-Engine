b427ae085a27de5d1462d29fd665e6787d1c6ce6
WSAL Evan <evan@millers6.com>
2024-11-17 09:42:33 -0500

    Added glfw and glad and tested them

--- engine/src/Application/Application.cpp
+++ engine/src/Application/Application.cpp
@@ -1,3 +1,6 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <WillowVoxEngine/Application/Application.h>
 #include <WillowVoxEngine/Events/EventDispatcher.h>
 #include <WillowVoxEngine/Events/WindowCloseEvent.h>
@@ -17,22 +20,44 @@ namespace WillowVox
 
 	void Application::Run()
 	{
-		EventDispatcher testDispatcher;
-		testDispatcher.RegisterListener(Event::Type::WindowClose, [this](Event& event) {
-			isRunning = false;
-			Logger::EngineLog("Window Closed\n");
-		});
+		// Test window code
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-		while (isRunning)
+		GLFWwindow* window = glfwCreateWindow(600, 400, "Test", nullptr, nullptr);
+		if (window == nullptr)
 		{
+			Logger::EngineLog("Failed to create window!\n");
+			glfwTerminate();
+			isRunning = false;
+		}
+		glfwMakeContextCurrent(window);
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
+			Logger::EngineLog("Failed to initialize GLAD\n");
+			isRunning = false;
+		}
+
+		glViewport(0, 0, 600, 400);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+		while (!glfwWindowShouldClose(window) && isRunning)
+		{
+			glClear(GL_COLOR_BUFFER_BIT);
+
 			// Run game logic
 			Update();
 
 			// Render the game
 			Render();
 
-			WindowCloseEvent windowCloseEvent;
-			testDispatcher.Dispatch(windowCloseEvent);
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
+
+		glfwTerminate();
 	}
 }
