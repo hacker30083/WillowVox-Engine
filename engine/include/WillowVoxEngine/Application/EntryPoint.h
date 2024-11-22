d43d53846e17e211a5b93ee0df0e06e8dacb1b4e
WSAL Evan <evan@millers6.com>
2024-11-22 16:55:06 -0500

    Cleaned up test code

--- engine/include/WillowVoxEngine/Application/EntryPoint.h
+++ engine/include/WillowVoxEngine/Application/EntryPoint.h
@@ -1,17 +1,8 @@
 #pragma once
 
 #include <WillowVoxEngine/Version.h>
-#include <iostream>
-#include <sstream>
 
 extern WillowVox::Application* WillowVox::CreateApplication();
-
-struct Vec3
-{
-	float x, y, z;
-
-	Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
-};
 
 int main(int argc, char** argv)
 {
