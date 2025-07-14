b0e47ce7f41762462a401aced28cce88f7a94fd0
Kaspar Aun <hacker30083@hotmail.com>
2025-07-14 19:58:37 +0300

    Added docs generation and updating the site.

--- engine/src/rendering/opengl/OpenGLShader.cpp
+++ engine/src/rendering/opengl/OpenGLShader.cpp
@@ -8,7 +8,14 @@
 #include <glm/gtc/type_ptr.hpp>
 
 namespace WillowVox
-{
+{   
+
+    /**
+     * Initializes shader from vertex and fragment shader files
+     * 
+     * @param vertexShaderPath Path to vertex shader file
+     * @param fragmentShaderPath Path to fragment shader file
+     */
 	OpenGLShader::OpenGLShader(const char* vertexShaderPath, const char* fragmentShaderPath)
 	{
         // 1. retrieve the vertex/fragment source code from filePath
@@ -89,6 +96,14 @@ namespace WillowVox
         glDeleteShader(fragment);
 	}
 
+
+    /**
+     * Creates shader from vertex and fragment shader code
+     * 
+     * @param vertexShaderCode Code for vertex shader (in glsl format)
+     * @param fragmentShaderCode Code for fragment shader (in glsl format)
+     * @param codePassed Not used
+     */
     OpenGLShader::OpenGLShader(const char* vertexShaderCode, const char* fragmentShaderCode, bool codePassed)
     {
         // 2. compile shaders
@@ -138,61 +153,133 @@ namespace WillowVox
         glDeleteShader(fragment);
     }
 
+    /**
+     * Delete shader program
+     */
     OpenGLShader::~OpenGLShader()
     {
         glDeleteProgram(_programId);
     }
 
+    /**
+     * Set the shader program as active to be used for rendering until another one is chosen
+     */
     void OpenGLShader::Bind()
     {
         glUseProgram(_programId);
     }
 
+    /**
+     * Set bool variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param value The value you want to set to the variable
+     */
     void OpenGLShader::SetBool(const char* name, bool value) const
     {
         glUniform1i(glGetUniformLocation(_programId, name), (int)value);
     }
 
+    /**
+     * Set int variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param value The value you want to set to the variable
+     */
     void OpenGLShader::SetInt(const char* name, int value) const
     {
         glUniform1i(glGetUniformLocation(_programId, name), value);
     }
 
+    /**
+     * Set float variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param value The value you want to set to the variable
+     */
     void OpenGLShader::SetFloat(const char* name, float value) const
     {
         glUniform1f(glGetUniformLocation(_programId, name), value);
     }
 
+    /**
+     * Set vec2 variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param value The value you want to set to the variable
+     */
     void OpenGLShader::SetVec2(const char* name, glm::vec2 value) const
     {
         glUniform2f(glGetUniformLocation(_programId, name), value.x, value.y);
     }
 
+    /**
+     * Set vec2 variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param x First part in vec2
+     * @param y Second part in vec2
+     */
     void OpenGLShader::SetVec2(const char* name, float x, float y) const
     {
         glUniform2f(glGetUniformLocation(_programId, name), x, y);
     }
 
+    /**
+     * Set vec3 variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param value The value you want to set to the variable
+     */
     void OpenGLShader::SetVec3(const char* name, glm::vec3 value) const
     {
         glUniform3f(glGetUniformLocation(_programId, name), value.x, value.y, value.z);
     }
 
+    /**
+     * Set vec3 variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param x First value in vec3
+     * @param y Second value in vec3
+     * @param z Third value in vec3
+     */
     void OpenGLShader::SetVec3(const char* name, float x, float y, float z) const
     {
         glUniform3f(glGetUniformLocation(_programId, name), x, y, z);
     }
 
+    /**
+     * Set vec4 variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param value The value you want to set to the variable
+     */
     void OpenGLShader::SetVec4(const char* name, glm::vec4 value) const
     {
         glUniform4f(glGetUniformLocation(_programId, name), value.x, value.y, value.z, value.w);
     }
-
+    
+    /**
+     * Set vec4 variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param x First value in vec4
+     * @param y Second value in vec4
+     * @param z Third value in vec4
+     * @param w Fourth value in vec4
+     */
     void OpenGLShader::SetVec4(const char* name, float x, float y, float z, float w) const
     {
         glUniform4f(glGetUniformLocation(_programId, name), x, y, z, w);
     }
 
+    /**
+     * Set mat4 variable (a.k.a uniform) in shader by name
+     * 
+     * @param name Name of the variable (uniform) in the shader
+     * @param value The value you want to set to the variable
+     */
     void OpenGLShader::SetMat4(const char* name, glm::mat4 value) const
     {
         glUniformMatrix4fv(glGetUniformLocation(_programId, name), 1, GL_FALSE, glm::value_ptr(value));
